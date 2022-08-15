f99713f11a5efca4b6eaef0638f07e8a26a3d23c
Viktoryia <viktoriabakun00@gmail.com>
2022-08-15 17:33:11 +0400

    test:
    add mockend.json

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,56 @@
 import type { FC } from 'react';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
 
-const Home: FC = () => (
-  <div className="homeContainer">
-    <Link className="link" to="/counter">
-      Counter
-    </Link>
-    <Link className="link" to="todo">
-      TodoList
-    </Link>
-  </div>
-);
+const Home: FC = () => {
+  const [todos, setTodos] = useState();
+
+  const getRes = useCallback(async () => {
+    await fetch('https://crudcrud.com/api/2c93691a3b514470a449d1ef670f7628/stuff')
+      .then((response) => response.json())
+      .then((json) => setTodos(json));
+  }, []);
+
+  const postStuff = useCallback(async () => {
+    await fetch('https://crudcrud.com/api/2c93691a3b514470a449d1ef670f7628/stuff', {
+      // Adding method type
+      method: 'POST',
+
+      // Adding body or contents to send
+      body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        id: 1,
+      }),
+
+      // Adding headers to the request
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  }, []);
+
+  return (
+    <div className="homeContainer">
+      <Link className="link" to="/counter">
+        Counter
+      </Link>
+      <Link className="link" to="todo">
+        TodoList
+      </Link>
+      <div>
+        list of todoLists:
+        <button type="button" onClick={getRes}>
+          get todos
+        </button>
+        <button type="button" onClick={postStuff}>
+          post stuff
+        </button>
+        <div>{todos}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
